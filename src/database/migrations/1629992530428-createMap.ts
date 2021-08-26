eca3a96d98642fb39301850d18254b1db76ab5be
Lucas Ribeiro <lucasrb04@gmail.com>
2021-08-26 13:40:38 -0300

    [ADD] Adicionando arquivos de migration, entity e repository referente ao mapa

---
+++ src/database/migrations/1629992530428-createMap.ts
@@ -0,0 +1,46 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createMap1629992530428 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'maps',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'font',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'description',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('maps');
+    }
+
+} 
