fa4b14aa7589c50b6e5a94a383e3171879dbb817
Lucas Ribeiro <lucasrb04@gmail.com>
2021-08-27 13:31:15 -0300

    [ADD] migrations, entity e repository dos predios

---
+++ src/entities/Building.ts
@@ -0,0 +1,34 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
+import { v4 as uuid } from 'uuid';
+
+@Entity('buildings')
+class Building {
+    @PrimaryColumn('uuid')
+    readonly id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    latitute: number;
+
+    @Column()
+    longitude: number;
+
+    @Column()
+    description: string;
+    
+    @CreateDateColumn()
+    created_at: Date
+
+    @UpdateDateColumn()
+    updated_at: Date
+
+    constructor() {
+        if(!this.id) {
+            this.id = uuid();
+        }
+    }
+}
+
+export { Building }
