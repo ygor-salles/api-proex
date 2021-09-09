d995661c89b868458e031add59e76ab0b85855bb
ygor-salles <ygorsallesaniceto@gmail.com>
2021-09-09 19:35:35 -0300

    feature/addFormatting-eslint-prettier

--- ormconfig.js
+++ ormconfig.js
@@ -1,63 +1,63 @@
 require('dotenv').config();
 
-let config = {}
+let config = {};
 if (process.env.NODE_ENV === 'development') {
-    config = {
-        type: 'postgres',
-        host: process.env.BD_HOST,
-        port: process.env.BD_PORT || 5432,
-        username: process.env.BD_USERNAME,
-        password: process.env.BD_PASSWORD,
-        database: process.env.BD_DATABASE,
-        synchronize: false,
-        migrations: ['src/database/migrations/*.ts'],
-        entities: ['src/entities/*.ts'],
-        cli: {
-            migrationsDir: 'src/database/migrations'
-        }
-    }
+  config = {
+    type: 'postgres',
+    host: process.env.BD_HOST,
+    port: process.env.BD_PORT || 5432,
+    username: process.env.BD_USERNAME,
+    password: process.env.BD_PASSWORD,
+    database: process.env.BD_DATABASE,
+    synchronize: false,
+    migrations: ['src/database/migrations/*.ts'],
+    entities: ['src/entities/*.ts'],
+    cli: {
+      migrationsDir: 'src/database/migrations',
+    },
+  };
 } else if (process.env.NODE_ENV === 'homolog') {
-    config = {
-        type: 'postgres',
-        host: process.env.BD_HOMOLOG_HOST,
-        port: process.env.BD_HOMOLOG_PORT || 5432,
-        username: process.env.BD_HOMOLOG_USERNAME,
-        password: process.env.BD_HOMOLOG_PASSWORD,
-        database: process.env.BD_HOMOLOG_DATABASE,
-        synchronize: false,
-        extra: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        },
-        migrations: ['build/database/migrations/*.js'],
-        entities: ['build/entities/*.js'],
-        cli: {
-            migrationsDir: 'build/database/migrations'
-        }
-    }
+  config = {
+    type: 'postgres',
+    host: process.env.BD_HOMOLOG_HOST,
+    port: process.env.BD_HOMOLOG_PORT || 5432,
+    username: process.env.BD_HOMOLOG_USERNAME,
+    password: process.env.BD_HOMOLOG_PASSWORD,
+    database: process.env.BD_HOMOLOG_DATABASE,
+    synchronize: false,
+    extra: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    migrations: ['build/database/migrations/*.js'],
+    entities: ['build/entities/*.js'],
+    cli: {
+      migrationsDir: 'build/database/migrations',
+    },
+  };
 } else if (process.env.NODE_ENV === 'production') {
-    config = {
-        type: 'postgres',
-        host: process.env.BD_PROD_HOST,
-        port: process.env.BD_PROD_PORT || 5432,
-        username: process.env.BD_PROD_USERNAME,
-        password: process.env.BD_PROD_PASSWORD,
-        database: process.env.BD_PROD_DATABASE,
-        synchronize: false,
-        extra: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        },
-        migrations: ['build/database/migrations/*.js'],
-        entities: ['build/entities/*.js'],
-        cli: {
-            migrationsDir: 'build/database/migrations'
-        }
-    }
+  config = {
+    type: 'postgres',
+    host: process.env.BD_PROD_HOST,
+    port: process.env.BD_PROD_PORT || 5432,
+    username: process.env.BD_PROD_USERNAME,
+    password: process.env.BD_PROD_PASSWORD,
+    database: process.env.BD_PROD_DATABASE,
+    synchronize: false,
+    extra: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    migrations: ['build/database/migrations/*.js'],
+    entities: ['build/entities/*.js'],
+    cli: {
+      migrationsDir: 'build/database/migrations',
+    },
+  };
 }
 
-module.exports = config
+module.exports = config;
