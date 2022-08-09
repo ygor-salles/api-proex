9c38cdb7e416e4eb3545f60c9b1743cb2f25601d
GabrielCorts <ccpereiragabriel@gmail.com>
2022-08-08 21:43:47 -0300

    refactor: adjusting Point Controller tests

--- src/__tests__/integration/PointController.spec.ts
+++ src/__tests__/integration/PointController.spec.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { getConnection, getCustomRepository } from 'typeorm';
 import { app } from '../../app';
 import createConnetion from '../../database';
+import { IPoint } from '../../interfaces/IPoint.interface';
 import { PointRepository } from '../../repositories/PointRepository';
 
 // ids de mapas cadastradas no seeders
@@ -11,25 +12,30 @@ const mapId2 = 'd80c3e0f-97b7-4a1d-bba3-384db2c1ff5c';
 // id inexistente
 const idInexist = 'bf918fbb-94a9-4dd5-9db1-85ce524ed306';
 
-const createPoint = {
+const createPoint : IPoint = {
   name: 'Point Test',
   description: 'Point Test Description',
   floor: 1,
-  altitude: 2.324,
-  latitude: -25.3347702,
-  longitude: -47.5304402,
-  isObstacle: true,
+  x: -25.3347702,
+  y: -47.5304402,
+  breakPoint: true,
+  neighbor: JSON.stringify({
+    right: "9fcbbe36-b52d-4192-8ff3-987049d7d9b3",
+    left: "4ae7a74c-b598-404d-8fc0-6fa7b8c5c822"
+  }),
   map_id: mapId1,
 };
 
 const editPoint = {
   name: 'Point Test Edited',
   description: 'Point Test Description Edited',
-  floor: 2,
-  altitude: 3.123,
-  latitude: -25.3347701,
-  longitude: -47.5304401,
-  isObstacle: false,
+  floor: 1,
+  x: -25.3347701,
+  y: -47.5304401,
+  breakPoint: false,
+  neighbor: JSON.stringify({
+    right: "9fcbbe36-b52d-4192-8ff3-987049d7d9b3",
+  }),
   map_id: mapId2,
 };
 
@@ -37,10 +43,12 @@ const pointInvalid = {
   name: 'Point Test 2',
   description: 'Point Test Description 2',
   floor: 2,
-  altitude: 2.321,
-  latitude: -25.3347701,
-  longitude: -47.5304401,
-  isObstacle: true,
+  x: -25.3347701,
+  y: -47.5304401,
+  breakPoint: true,
+  neighbor: JSON.stringify({
+    left: "9fcbbe36-b52d-4192-8ff3-987049d7d9b3",
+  }),
   map_id: idInexist,
 };
 
@@ -78,9 +86,11 @@ describe('Maps', () => {
     expect(response.body.name).toBe(createPoint.name);
     expect(response.body.description).toBe(createPoint.description);
     expect(response.body.floor).toBe(createPoint.floor);
-    expect(response.body.altitude).toBe(createPoint.altitude);
-    expect(response.body.latitude).toBe(createPoint.latitude);
-    expect(response.body.longitude).toBe(createPoint.longitude);
+    expect(response.body.x).toBe(createPoint.x);
+    expect(response.body.y).toBe(createPoint.y);
+    expect(response.body.breakPoint).toBe(createPoint.breakPoint);
+    expect(response.body.neighbor).toBe(createPoint.neighbor);
+    expect(response.body.map_id).toBe(createPoint.map_id);
   });
 
   it('Should returns 400 beacause there is no point name', async () => {
@@ -90,10 +100,13 @@ describe('Maps', () => {
       .send({
         description: 'Point Test Description',
         floor: 1,
-        altitude: 2.324,
-        latitude: -25.3347702,
-        longitude: -47.5304402,
-        isObstacle: true,
+        x: -25.3347702,
+        y: -47.5304402,
+        breakPoint: true,
+        neighbor: JSON.stringify({
+          right: "9fcbbe36-b52d-4192-8ff3-987049d7d9b3",
+          left: "4ae7a74c-b598-404d-8fc0-6fa7b8c5c822"
+        }),
         map_id: mapId1,
       });
 
@@ -108,10 +121,13 @@ describe('Maps', () => {
       .send({
         name: 'Point Test',
         floor: 1,
-        altitude: 2.324,
-        latitude: -25.3347702,
-        longitude: -47.5304402,
-        isObstacle: true,
+        x: -25.3347702,
+        y: -47.5304402,
+        breakPoint: true,
+        neighbor: JSON.stringify({
+          right: "9fcbbe36-b52d-4192-8ff3-987049d7d9b3",
+          left: "4ae7a74c-b598-404d-8fc0-6fa7b8c5c822"
+        }),
         map_id: mapId1,
       });
 
@@ -126,10 +142,13 @@ describe('Maps', () => {
       .send({
         name: 'Point Test',
         description: 'Point Test Description',
-        altitude: 2.324,
-        latitude: -25.3347702,
-        longitude: -47.5304402,
-        isObstacle: true,
+        x: -25.3347702,
+        y: -47.5304402,
+        breakPoint: true,
+        neighbor: JSON.stringify({
+          right: "9fcbbe36-b52d-4192-8ff3-987049d7d9b3",
+          left: "4ae7a74c-b598-404d-8fc0-6fa7b8c5c822"
+        }),
         map_id: mapId1,
       });
 
@@ -137,7 +156,7 @@ describe('Maps', () => {
     expect(response.body.message).toBe('O Andar é obrigatório');
   });
 
-  it('Should returns 400 beacause there is no point altitude', async () => {
+  it('Should returns 400 beacause there is no point x', async () => {
     const response = await request(app)
       .post('/points')
       .set('Authorization', `bearer ${token}`)
@@ -145,17 +164,20 @@ describe('Maps', () => {
         name: 'Point Test',
         description: 'Point Test Description',
         floor: 1,
-        latitude: -25.3347702,
-        longitude: -47.5304402,
-        isObstacle: true,
+        y: -47.5304402,
+        breakPoint: true,
+        neighbor: JSON.stringify({
+          right: "9fcbbe36-b52d-4192-8ff3-987049d7d9b3",
+          left: "4ae7a74c-b598-404d-8fc0-6fa7b8c5c822"
+        }),
         map_id: mapId1,
       });
 
     expect(response.status).toBe(400);
-    expect(response.body.message).toBe('A Altitude é obrigatória');
+    expect(response.body.message).toBe('A coordenada x é obrigatória');
   });
 
-  it('Should returns 400 beacause there is no point latitude', async () => {
+  it('Should returns 400 beacause there is no point y', async () => {
     const response = await request(app)
       .post('/points')
       .set('Authorization', `bearer ${token}`)
@@ -163,17 +185,20 @@ describe('Maps', () => {
         name: 'Point Test',
         description: 'Point Test Description',
         floor: 1,
-        altitude: 2.324,
-        longitude: -47.5304402,
-        isObstacle: true,
+        x: -25.3347702,
+        breakPoint: true,
+        neighbor: JSON.stringify({
+          right: "9fcbbe36-b52d-4192-8ff3-987049d7d9b3",
+          left: "4ae7a74c-b598-404d-8fc0-6fa7b8c5c822"
+        }),
         map_id: mapId1,
       });
 
     expect(response.status).toBe(400);
-    expect(response.body.message).toBe('A Latitude é obrigatória');
+    expect(response.body.message).toBe('A coordenada y é obrigatória');
   });
 
-  it('Should returns 400 beacause there is no point longitude', async () => {
+  it('Should returns 400 beacause there is no point break point', async () => {
     const response = await request(app)
       .post('/points')
       .set('Authorization', `bearer ${token}`)
@@ -181,17 +206,20 @@ describe('Maps', () => {
         name: 'Point Test',
         description: 'Point Test Description',
         floor: 1,
-        altitude: 2.324,
-        latitude: -25.3347702,
-        isObstacle: true,
+        x: -25.3347702,
+        y: -47.5304402,
+        neighbor: JSON.stringify({
+          right: "9fcbbe36-b52d-4192-8ff3-987049d7d9b3",
+          left: "4ae7a74c-b598-404d-8fc0-6fa7b8c5c822"
+        }),
         map_id: mapId1,
       });
 
     expect(response.status).toBe(400);
-    expect(response.body.message).toBe('A Longitude é obrigatória');
+    expect(response.body.message).toBe('O Break Point é obrigatório');
   });
 
-  it('Should returns 400 beacause there is no point obstacle', async () => {
+  it('Should returns 400 beacause there is no point neighbor', async () => {
     const response = await request(app)
       .post('/points')
       .set('Authorization', `bearer ${token}`)
@@ -199,14 +227,33 @@ describe('Maps', () => {
         name: 'Point Test',
         description: 'Point Test Description',
         floor: 1,
-        altitude: 2.324,
-        latitude: -25.3347702,
-        longitude: -47.5304402,
+        x: -25.3347702,
+        y: -47.5304402,
+        breakPoint: true,
         map_id: mapId1,
       });
 
     expect(response.status).toBe(400);
-    expect(response.body.message).toBe('O obstáculo é obrigatório');
+    expect(response.body.message).toBe('Os vizinhos são obrigatórios');
+  });
+
+  it('Should returns 400 beacause neighbor is not a valid JSON', async () => {
+    const response = await request(app)
+      .post('/points')
+      .set('Authorization', `bearer ${token}`)
+      .send({
+        name: 'Point Test',
+        description: 'Point Test Description',
+        floor: 1,
+        x: -25.3347702,
+        y: -47.5304402,
+        breakPoint: true,
+        neighbor: "{notJSON}",
+        map_id: mapId1,
+      });
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe('Os vizinhos não estão em formato JSON');
   });
 
   it('Should returns 400 beacause there is no point map_id', async () => {
@@ -217,17 +264,20 @@ describe('Maps', () => {
         name: 'Point Test',
         description: 'Point Test Description',
         floor: 1,
-        altitude: 2.324,
-        latitude: -25.3347702,
-        longitude: -47.5304402,
-        isObstacle: true,
+        x: -25.3347702,
+        y: -47.5304402,
+        breakPoint: true,
+        neighbor: JSON.stringify({
+          right: "9fcbbe36-b52d-4192-8ff3-987049d7d9b3",
+          left: "4ae7a74c-b598-404d-8fc0-6fa7b8c5c822"
+        }),
       });
 
     expect(response.status).toBe(400);
     expect(response.body.message).toBe('Id de mapa é obrigatório');
   });
 
-  it('Should not be able to create a point with exists and return 400', async () => {
+  it('Should not be able to create a point that already exists and return 400', async () => {
     const response = await request(app)
       .post('/points')
       .set('Authorization', `bearer ${token}`)
@@ -261,10 +311,10 @@ describe('Maps', () => {
     expect(editPoint.name).toBe(pointUpdated.name);
     expect(editPoint.description).toBe(pointUpdated.description);
     expect(editPoint.floor).toBe(pointUpdated.floor);
-    expect(editPoint.altitude).toBe(pointUpdated.altitude);
-    expect(editPoint.latitude).toBe(pointUpdated.latitude);
-    expect(editPoint.longitude).toBe(pointUpdated.longitude);
-    expect(editPoint.isObstacle).toBe(pointUpdated.isObstacle);
+    expect(editPoint.x).toBe(pointUpdated.x);
+    expect(editPoint.y).toBe(pointUpdated.y);
+    expect(editPoint.breakPoint).toBe(pointUpdated.breakPoint);
+    expect(editPoint.neighbor).toBe(pointUpdated.neighbor);
     expect(editPoint.map_id).toBe(pointUpdated.map_id);
     expect(response.body.message).toBe('Ponto atualizado com sucesso!');
   });
@@ -299,6 +349,25 @@ describe('Maps', () => {
     expect(response.body.message).toBe('Ponto não existe!');
   });
 
+  it('Should returns 400 beacause neighbor is not a valid JSON', async () => {
+    const response = await request(app)
+      .put(`/points/${pointId}`)
+      .set('Authorization', `bearer ${token}`)
+      .send({
+        name: 'Point Test',
+        description: 'Point Test Description',
+        floor: 1,
+        x: -25.3347702,
+        y: -47.5304402,
+        breakPoint: true,
+        neighbor: "{notJSON}",
+        map_id: mapId1,
+      });
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe('Os vizinhos não estão em formato JSON');
+  });
+
   // testes para visualição de ponto por id
   it('Should be able to get a point by Id and return 200', async () => {
     const response = await request(app)
@@ -309,10 +378,10 @@ describe('Maps', () => {
     expect(response.body.name).toBe(editPoint.name);
     expect(response.body.description).toBe(editPoint.description);
     expect(response.body.floor).toBe(editPoint.floor);
-    expect(response.body.altitude).toBe(editPoint.altitude);
-    expect(response.body.latitude).toBe(editPoint.latitude);
-    expect(response.body.longitude).toBe(editPoint.longitude);
-    expect(response.body.isObstacle).toBe(editPoint.isObstacle);
+    expect(response.body.x).toBe(editPoint.x);
+    expect(response.body.y).toBe(editPoint.y);
+    expect(response.body.breakPoint).toBe(editPoint.breakPoint);
+    expect(response.body.neighbor).toBe(editPoint.neighbor);
     expect(response.body.map_id).toBe(editPoint.map_id);
   });
 
